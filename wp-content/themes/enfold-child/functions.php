db1fe29073a49883d5a84536f85b759f98dfaa90
irthos <irth03@gmail.com>
2015-01-09 21:21:19 -0800

    better wp

--- wp-content/themes/enfold-child/functions.php
+++ wp-content/themes/enfold-child/functions.php
@@ -12,9 +12,9 @@ function current_userFn() {
      */
     echo 'Username: ' . $current_user->user_login . '<br />';
     echo 'User email: ' . $current_user->user_email . '<br />';
-    echo 'User first name: ' . $current_user->user_firstname . '<br />';
-    echo 'User last name: ' . $current_user->user_lastname . '<br />';
-    echo 'User display name: ' . $current_user->display_name . '<br />';
-    echo 'User ID: ' . $current_user->ID . '<br />';
+    // echo 'User first name: ' . $current_user->user_firstname . '<br />';
+    // echo 'User last name: ' . $current_user->user_lastname . '<br />';
+    // echo 'User display name: ' . $current_user->display_name . '<br />';
+    // echo 'User ID: ' . $current_user->ID . '<br />';
 }
 add_shortcode( 'current_user', 'current_userFn' );
