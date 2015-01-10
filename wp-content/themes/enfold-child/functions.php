9db57e09b7f92b4bd9958699cac5fd74f87b33a5
irthos <irth03@gmail.com>
2015-01-09 20:48:29 -0800

    remove old geod

---
+++ wp-content/themes/enfold-child/functions.php
@@ -0,0 +1,20 @@
+// Add Shortcode
+function current_user() {
+
+	// Code
+<?php
+    $current_user = wp_get_current_user();
+    /**
+     * @example Safe usage: $current_user = wp_get_current_user();
+     * if ( !($current_user instanceof WP_User) )
+     *     return;
+     */
+    echo 'Username: ' . $current_user->user_login . '<br />';
+    echo 'User email: ' . $current_user->user_email . '<br />';
+    echo 'User first name: ' . $current_user->user_firstname . '<br />';
+    echo 'User last name: ' . $current_user->user_lastname . '<br />';
+    echo 'User display name: ' . $current_user->display_name . '<br />';
+    echo 'User ID: ' . $current_user->ID . '<br />';
+?>
+}
+add_shortcode( 'current_user', 'current_user' );
